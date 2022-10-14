ecbbdb35e1de7e70d36e641b6e593268006c8803
N2am <nouranmahmoud321@gmail.com>
2022-10-14 17:25:22 +0200

    feat: create weather module

---
+++ src/app/contants.ts
@@ -0,0 +1,2 @@
+export const API_KEY = '7518e07dfc754fc58ac134653221410';
+export const API_URL = 'http://api.weatherapi.com/v1'
--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WeatherComponent } from './components/weather.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WeatherComponent
+  ],
   imports: [
     CommonModule
   ]
